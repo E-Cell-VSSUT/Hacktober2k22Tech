4b5e870338281f827f39cbf7c25595acb974b1bd
Kenil <kenilshah1505@gmail.com>
2022-10-08 23:21:48 +0530

    All files added

---
+++ newweather/src/App.js
@@ -0,0 +1,71 @@
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css"
+import { useEffect, useState } from "react";
+import './App.css';
+//jdh
+
+
+function App() {
+
+
+  const apiKey = "3853bdd39880acc77e71cc41405a41e1"
+  const [inputCity, setInputCity] = useState("")
+  const [data, setData] = useState({})
+
+
+  const getWetherDetails = (cityName) => {
+    if (!cityName) return
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
+    axios.get(apiURL).then((res) => {
+      console.log("response", res.data)
+      setData(res.data)
+    }).catch((err) => {
+      console.log("err", err)
+    })
+  }
+
+  const handleChangeInput = (e) => {
+    console.log("value", e.target.value)
+    setInputCity(e.target.value)
+  }
+
+  const handleSearch = () => {
+    getWetherDetails(inputCity)
+  }
+
+
+  return (
+    <div className="col-md-12">
+      <div className="wetherBg">
+        <h1 className="heading">Weather App</h1>
+
+        <div className="d-grid gap-3 col-4 mt-4">
+          <input type="text" className="form-control"
+            value={inputCity}
+            onChange={handleChangeInput} />
+          <button className="btn btn-primary" type="button"
+            onClick={handleSearch}
+          >Search</button>
+        </div>
+      </div>
+
+      {Object.keys(data).length > 0 &&
+        <div className="col-md-12 text-center mt-5">
+
+          <div className="shadow rounded wetherResultBox">
+            <img className="weathorIcon"
+              src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png" />
+
+            <h5 className="weathorCity">
+              {data?.name}
+            </h5>
+            <h6 className="weathorTemp">{((data?.main?.temp) - 273.15).toFixed(2)}°C</h6>
+          </div>
+        </div>
+      }
+
+    </div>
+  );
+}
+
+export default App;
