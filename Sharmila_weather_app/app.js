1668d5b2490f93d810fece0614566f0f12c53aea
Sharmila_21 <shamuu2002@gmail.com>
2022-10-30 11:44:42 +0530

    Sharmila_weather_app added

--- Sharmila_weather_app/app.js
+++ Sharmila_weather_app/app.js
@@ -45,23 +45,5 @@ app.listen(5000,function(){
     console.log("your server is running at port 5000");
 });
 
-// var query="london";
-// var apikey= "7e34aa1b1b97fe8e6e0233e91782bfb8";
-// var unit="metric";
 
-//  url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apikey+"&units="+unit;
-//  https.get(url,function(response){
-//    console.log(response.statusCode);
-//    response.on('data',function(data){
-//      const  weatherdata=JSON.parse(data);
-     
-//     const temp=weatherdata.main.temp;
-//    const  descrp=weatherdata.weather[0].description;
-//      res.write("<p> weather description " +descrp+"</p>");
-//      res.write("<h1>The temperature in london is "+temp+" degree celsius</h1>");
-//      res.write(`<img src="https://openweathermap.org/img/wn/10d@2x.png">`);
-//     res.send();
-     
 
-//    });
-//  });
